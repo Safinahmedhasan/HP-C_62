6ffc02a424c103856c7ab4fb5dea14d03cb69f5c
safinahmedhasan <safinahmedhaasan@gmail.com>
2024-12-28 12:27:40 +0600

    added

---
+++ Math.js
@@ -0,0 +1,13 @@
+/**
+ *  MATH Needed For programming
+ * 
+ * 1. Number +ve , -ve 0 [1,2,3,4,5,6] -60
+ * 2. integer 2,4,5, 
+ * 3. float 2.4, 4.6
+ * 4. Operation + - * / %
+ * 5. 5%
+ * 
+ * .sum 2+2 = 4
+ * Avg: 4,5,7
+ *  
+ **/
---
+++ script.js
@@ -0,0 +1,29 @@
+
+
+const sum = 9+0;
+console.log(sum);
+
+// alert(sum)
+// document.getElementById('text').innerHTML = sum
+
+if(sum > 50){
+    document.getElementById('text').innerHTML = "Right"
+}else{
+    document.getElementById('text').innerHTML = "Wrong"
+}
+
+function HayHello(name){
+    console.log(`${name} dhfdjfh`);
+}
+HayHello("HI")
+
+
+
+// Single Line Comment
+/*
+Multi 
+Line
+Comment
+*/
+
+
